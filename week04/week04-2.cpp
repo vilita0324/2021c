5e3839907954859164fcf19dd36b74a17645daaf
vilita0324 <91250471+vilita0324@users.noreply.github.com>
2021-10-07 17:38:28 +0800

    Add files via upload

---
+++ week04/week04-2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float a=3.14159265358979;
+    double b=3.14159265358979;
+    printf("%f\n",a);
+    printf("%d\n",b);
+    printf("%.4f\n",b);
+    printf("%.9f\n",b);
+    printf("%.13f\n",b);
+}
